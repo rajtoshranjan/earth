e228df5eac0edf6ea3bb8fb5d4749b6abbe76c7b
Ashish Singh <ashishsingh@Rajtosh.local>
2023-03-20 22:06:16 +0530

    maplibre config

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./assets/scss/styles.scss";
+import { Map } from "maplibre-gl";
+import { useEffect, useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const mapContainerRef = useRef(null);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  useEffect(() => {
+    if (!mapContainerRef.current) {
+      return;
+    }
+
+    const map = new Map({
+      container: mapContainerRef.current,
+      style: "https://demotiles.maplibre.org/style.json", // stylesheet location
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }, []);
+
+  return <div ref={mapContainerRef} className="map-container" />;
 }
 
-export default App
+export default App;
