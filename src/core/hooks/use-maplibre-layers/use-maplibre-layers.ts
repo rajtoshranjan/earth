c6aff0c760433460a40c869d2b64683e073630d3
Rajtosh Ranjan <rajtoshranjan@gmail.com>
2025-05-25 16:41:43 +0530

    Bump use-idb-store version

--- src/core/hooks/use-maplibre-layers/use-maplibre-layers.ts
+++ src/core/hooks/use-maplibre-layers/use-maplibre-layers.ts
@@ -1,54 +1,32 @@
-import { useEffect, useMemo } from 'react';
+import { useMemo } from 'react';
 import { useIndexedDbStore } from 'use-idb-store';
-import { useLocalStorage } from 'usehooks-ts';
 import { Map } from '../../maplibre';
 import { LayerManager } from './layer-manager';
-import { LayerInfo, Layers } from './types';
+import { LayerInfo } from './types';
 
-export const useMaplibreLayers = (map: Map) => {
+export type MaplibreLayerHookRes = {
+  layers: Record<string, LayerInfo>;
+  layerManager: LayerManager;
+  isLoading: boolean;
+};
+
+export const useMaplibreLayers = (map: Map): MaplibreLayerHookRes => {
   // State.
   const {
     values: layers,
     mutations,
     isLoading,
+    isReady,
   } = useIndexedDbStore<LayerInfo>('layers');
 
-  // ** Migration start **
-  /**
-   * Migration from localStorage to IndexedDB
-   *
-   * This code handles the migration of layer data from localStorage to IndexedDB.
-   * It will be removed in the next major release.
-   */
-  const [oldLayers, setOldLayers] = useLocalStorage<Layers>('layers', {});
-
-  useEffect(() => {
-    const migrateLayersToIndexedDB = async () => {
-      if (!oldLayers || isLoading) return;
-
-      // Migrate each layer from localStorage to IndexedDB
-      for (const [id, layer] of Object.entries(oldLayers)) {
-        await mutations.addValue(id, layer);
-      }
-
-      // Clear localStorage after migration
-      setOldLayers({});
-    };
-
-    migrateLayersToIndexedDB();
-  }, [oldLayers, isLoading]);
-  // ** Migration end **
-
   // Manager.
-  const layerManager = useMemo(
-    () =>
-      new LayerManager({
-        map,
-        initialLayers: layers,
-        layerStoreMutations: mutations,
-      }),
-    [map, isLoading],
-  );
+  const layerManager = useMemo(() => {
+    return new LayerManager({
+      map,
+      initialLayers: layers,
+      layerStoreMutations: mutations,
+    });
+  }, [map, isReady]);
 
   return {
     layers,
